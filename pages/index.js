994fb8ff6f142b7c2699f981bbb9df7239e099fa
wnpraf <rapribu@gmail.com>
2020-01-22 13:24:16 +0100

    Delete dead dependency

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import Selector from "../components/selector"
 import SearchButton from "../components/SearchButton"
 import ClearButton from "../components/ClearButton"
