526e3f0ed5119ec397f12013e6c4102db0667572
wnpraf <rapribu@gmail.com>
2020-01-22 19:49:08 +0100

    Add select element styles

--- components/selector.js
+++ components/selector.js
@@ -1,14 +1,14 @@
 import React from "react"
-
+import { FaCaretDown } from "react-icons/fa"
 export default ({ stateLabel, infoLabel, data, filters, setFilters }) => {
   const { formFilters, isNotFiltered } = filters
   return (
-    <div className="py-3 sm:w-1/2 sm:px-2 lg:w-1/4">
+    <div className="py-3 sm:w-1/2 sm:px-2 lg:w-1/4 relative">
       <label className="capitalize pl-2" htmlFor={stateLabel}>
         {infoLabel}
       </label>
       <select
-        className=" block w-full  mt-2"
+        className=" block w-full  mt-2 py-1 pl-3 border border-gray-500 select-shadow cursor-pointer bg-gray-200 select-gradient focus:outline-none"
         onChange={e => {
           console.log("selected", e.target.value)
           setFilters({
@@ -31,6 +31,9 @@ export default ({ stateLabel, infoLabel, data, filters, setFilters }) => {
           )
         })}
       </select>
+      <i className="absolute position-right-middle">
+        <FaCaretDown />
+      </i>
     </div>
   )
 }
